9107e0eb91f8f8508e442ebe4317a554acf7d75f
SebasXeon <juansebastian727@gmail.com>
2026-05-02 17:11:23 -0500

    feat(ocr): add gRPC client wrapper

---
+++ internal/ocr/client.go
@@ -0,0 +1,66 @@
+package ocr
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"SebasXeon/Fileoteca/internal/ocr/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type OcrClient struct {
+	conn   *grpc.ClientConn
+	client proto.OCREngineClient
+	addr   string
+}
+
+func NewOcrClient(addr string) (*OcrClient, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	dialCtx, dialCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer dialCancel()
+
+	conn, err := grpc.DialContext(dialCtx, addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to OCR server at %s: %w", addr, err)
+	}
+
+	client := proto.NewOCREngineClient(conn)
+	log.Printf("connected to OCR server at %s", addr)
+	_ = ctx
+
+	return &OcrClient{
+		conn:   conn,
+		client: client,
+		addr:   addr,
+	}, nil
+}
+
+func (c *OcrClient) ExtractText(ctx context.Context, id, filePath, fileType string) (string, error) {
+	req := &proto.ExtractRequest{
+		Id:       id,
+		FilePath: filePath,
+		FileType: fileType,
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 60*time.Second)
+	defer cancel()
+
+	resp, err := c.client.ExtractText(ctx, req)
+	if err != nil {
+		return "", fmt.Errorf("OCR ExtractText failed for %s: %w", id, err)
+	}
+
+	return resp.Text, nil
+}
+
+func (c *OcrClient) Close() error {
+	return c.conn.Close()
+}
